321e01cc1ed1008c7269c0e434dda021ca49dc63
Antônio Jr <antonio.silvajr23@gmail.com>
2016-03-08 23:19:20 -0300

    Commit teste

---
+++ app/PetCare/app/src/main/java/cap7/com/br/petcare/activity/ProprietarioActivity.java
@@ -0,0 +1,15 @@
+package cap7.com.br.petcare.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cap7.com.br.petcare.R;
+
+public class ProprietarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proprietario);
+    }
+}
