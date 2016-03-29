f16b1868dea2ba715a7645d9f722de91547b6911
Antônio Jr <antonio.silvajr23@gmail.com>
2016-03-28 22:51:39 -0300

    Sprint 1 -SplashScreen

---
+++ app/PetCare/app/src/main/java/cap7/com/br/petcare/activity/SplashActivity.java
@@ -0,0 +1,26 @@
+package cap7.com.br.petcare.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import cap7.com.br.petcare.R;
+
+public class SplashActivity extends AppCompatActivity
+        implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 3000);
+    }
+
+    public void run(){
+        startActivity(new Intent(this, ProprietarioActivity.class));
+        finish();
+    }
+}
